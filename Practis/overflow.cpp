73fb3cd4d18bc1269f006f0ba62305375b03822b
yuki.anzai <dev.off.white@gmail.com>
2020-11-16 14:35:54 +0900

    test overflow

---
+++ Practis/overflow.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    cout << (100000 * 100000) << endl;
+    int x=100000;
+    cout << (x*x) << endl;
+    long y=x*x;
+    cout << y << endl;
+    long z=x*(long)x;
+    cout << z << endl;    
+}

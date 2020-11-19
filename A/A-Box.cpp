8020ff793c05e6ec2acea31e6f7546d91b0676b2
yuki.anzai <dev.off.white@gmail.com>
2020-11-19 13:56:24 +0900

    Success A-Box

---
+++ A/A-Box.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    int N, A, B;
+    cin >> N >> A >> B;
+
+    cout << N - A + B << endl;
+}

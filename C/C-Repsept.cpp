b6f3000bcdd20bf27f31120d892aed5cc87b44a2
yuki.anzai <dev.off.white@gmail.com>
2020-11-26 09:50:09 +0900

    Success C-Repsept

---
+++ C/C-Repsept.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    long long K;
+    cin >> K;
+
+    int ans = 7;
+    for (int i = 0; i < K; ++i) {
+      if (ans == 0) {
+        cout << i + 1 << endl;
+        return 0;
+      }
+      ans = (ans * 10 + 7) % K;
+    }
+
+    cout << "-1" << endl;
+    return 0;
+}

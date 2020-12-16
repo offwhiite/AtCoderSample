fdb7225311e99fc30c296e2c1db83d4df4618c4a
yuki.anzai <dev.off.white@gmail.com>
2020-12-17 06:09:24 +0900

    Success C-Vacation.cpp

--- Practis/DP/C-Vacation.cpp
+++ Practis/DP/C-Vacation.cpp
@@ -10,19 +10,23 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; }
 const long long INF = 1LL << 60;
 
 int main() {
-    int N, K;
-    cin >> N >> K;
-    vector <long long> v(N+1000,0);
-    for (int i = 0; i < N; ++i) cin >> v[i];
-    vector <long long> dp(N+1000, INF);
-    dp[0] = 0; // 一段目
+    int N;
+    cin >> N;
+    vector < vector<long long> > v(N, vector<long long>(3,0));
+    for (int i = 0; i < N; ++i) cin >> v[i][0] >> v[i][1] >> v[i][2]; 
+    vector < vector<long long> > dp(N+1, vector<long long>(3,0));
+    dp[0][0] = 0;
+    dp[0][1] = 0;
+    dp[0][2] = 0;// 一段目
+
     for (int i = 0; i < N; ++i) {
-        for (int j = 1; j <= K; ++j) {
-            // 一段目からいける範囲の物をdpにメモしていく
-            // 次は2段目からいける範囲の物で小さいものをメモしていくのを繰り返す
-            chmin(dp[i + j], dp[i] + abs(v[i+j] - v[i]));
-        }
+        chmax(dp[i+1][0], dp[i][1] + v[i][0]);
+        chmax(dp[i+1][0], dp[i][2] + v[i][0]);
+        chmax(dp[i+1][1], dp[i][0] + v[i][1]);
+        chmax(dp[i+1][1], dp[i][2] + v[i][1]);
+        chmax(dp[i+1][2], dp[i][0] + v[i][2]);
+        chmax(dp[i+1][2], dp[i][1] + v[i][2]);
     }
-    cout  << dp[N-1] << endl; // N段目はN-1段目
+    cout  << max(dp[N][0], max(dp[N][1], dp[N][2])) << endl; 
     return 0;
 }

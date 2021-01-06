6ae2f972840717b075c1b01b15cb4f19b8e8c1d5
yuki.anzai <dev.off.white@gmail.com>
2021-01-06 12:04:03 +0900

    A-Beginner.cpp

---
+++ ABC/A/A-Beginner.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int N, R;
+    cin >> N >> R;
+    if (N >= 10) {
+        cout << R;
+    } else {
+        cout << R + (100 * (10 - N));
+    }    
+    cout  << endl;
+    return 0;
+}

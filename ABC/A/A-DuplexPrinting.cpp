2fa2a060f57d9a72841469bc76493f39fc954407
yuki.anzai <dev.off.white@gmail.com>
2021-01-06 12:04:12 +0900

    A-DuplexPrint

---
+++ ABC/A/A-DuplexPrinting.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    cout  << N/2 + N%2 << endl;
+    return 0;
+}

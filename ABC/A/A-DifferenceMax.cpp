c72086bce468bc538bf7d41950908af717ec0c0e
yuki.anzai <dev.off.white@gmail.com>
2021-03-24 05:59:07 +0900

    Success A-DifferenceMax

---
+++ ABC/A/A-DifferenceMax.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <iomanip>
+#include <queue>
+using namespace std;
+
+int main() {
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+    cout  << b-c << endl;
+    return 0;
+}

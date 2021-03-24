96fc53b559548383e627005936b8655849307755
yuki.anzai <dev.off.white@gmail.com>
2021-03-25 05:32:14 +0900

    Succss ACorWA

---
+++ ABC/A/A-ACorWA.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <iomanip>
+#include <queue>
+using namespace std;
+
+int main() {
+    int N,M;
+    cin >> N >> M;
+    if (N==M) cout << "Yes";
+    else cout << "No";
+    
+    cout << endl;
+    return 0;
+}

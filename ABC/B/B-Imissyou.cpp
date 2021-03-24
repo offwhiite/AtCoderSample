ada4f0b3538dde8fc2befc16dba6f05c97dca2af
yuki.anzai <dev.off.white@gmail.com>
2021-03-25 05:33:41 +0900

    Succss B-Imissyou

---
+++ ABC/B/B-Imissyou.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <iomanip>
+#include <queue>
+using namespace std;
+
+int main() {
+    string N; cin >> N;
+    for (int i = 0; i < N.length(); ++i) N[i] = 'x';
+    cout << N << endl;
+    return 0;
+}

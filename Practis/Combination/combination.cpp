6c48f056c6440eaf5985cfbf32e999d2d43fd9ed
yuki.anzai <dev.off.white@gmail.com>
2021-04-02 06:10:08 +0900

    Success Number

---
+++ Practis/Combination/combination.cpp
@@ -0,0 +1,13 @@
+std::vector<std::vector<long long>> comb(int n, int r) {
+  std::vector<std::vector<long long>> v(n + 1,std::vector<long long>(n + 1, 0));
+  for (int i = 0; i < v.size(); i++) {
+    v[i][0] = 1;
+    v[i][i] = 1;
+  }
+  for (int j = 1; j < v.size(); j++) {
+    for (int k = 1; k < j; k++) {
+      v[j][k] = (v[j - 1][k - 1] + v[j - 1][k]);
+    }
+  }
+  return v;
+}
